efcba860145b95ca83f9b6e71f718908834d6f45
HenryB <henry@bahlinc.com>
2026-06-03 16:07:38 -0400

    Fix cmd issue

--- internal/externalcmd/cmd.go
+++ internal/externalcmd/cmd.go
@@ -1,110 +1,137 @@
-// Package externalcmd allows to launch external commands.
+// Package externalcmd allows launching external commands
 package externalcmd
 
 import (
-	"errors"
-	"fmt"
-	"os"
-	"time"
+    "context"
+    "errors"
+    "fmt"
+    "os"
+    "sync"
+    "time"
 )
 
 const (
-	restartPause = 5 * time.Second
+    restartPause = 5 * time.Second
 )
 
-var errTerminated = errors.New("terminated")
+var (
+    // errTerminated is returned when the command is terminated
+    errTerminated = errors.New("terminated")
+)
 
-// OnExitFunc is the prototype of onExit.
+// OnExitFunc is the prototype of onExit
 type OnExitFunc func(error)
 
-// Environment is a Cmd environment.
+// Environment is a Cmd environment
 type Environment map[string]string
 
-// Cmd is an external command.
+// Cmd is an external command
 type Cmd struct {
-	pool    *Pool
-	cmdstr  string
-	restart bool
-	env     Environment
-	onExit  func(error)
-
-	// in
-	terminate chan struct{}
+    pool      *Pool
+    cmdstr    string
+    restart   bool
+    env       Environment
+    onExit    func(error)
+    terminate chan struct{}
+    ctx       context.Context
+    cancel    context.CancelFunc
+    mu        sync.Mutex
+    running   bool
 }
 
-// NewCmd allocates a Cmd.
+// NewCmd allocates a Cmd
 func NewCmd(
-	pool *Pool,
-	cmdstr string,
-	restart bool,
-	env Environment,
-	onExit OnExitFunc,
+    pool *Pool,
+    cmdstr string,
+    restart bool,
+    env Environment,
+    onExit OnExitFunc,
 ) *Cmd {
-	// replace variables in both Linux and Windows, in order to allow using the
-	// same commands on both of them.
-	cmdstr = os.Expand(cmdstr, func(variable string) string {
-		if value, ok := env[variable]; ok {
-			return value
-		}
-		return os.Getenv(variable)
-	})
-
-	if onExit == nil {
-		onExit = func(_ error) {}
-	}
-
-	e := &Cmd{
-		pool:      pool,
-		cmdstr:    cmdstr,
-		restart:   restart,
-		env:       env,
-		onExit:    onExit,
-		terminate: make(chan struct{}),
-	}
-
-	pool.wg.Add(1)
-
-	go e.run()
-
-	return e
+    // Create a context that we can use to manage the lifecycle
+    ctx, cancel := context.WithCancel(context.Background())
+    
+    // Expand environment variables
+    cmdstr = os.Expand(cmdstr, func(variable string) string {
+        if value, ok := env[variable]; ok {
+            return value
+        }
+        return os.Getenv(variable)
+    })
+    
+    if onExit == nil {
+        onExit = func(_ error) {}
+    }
+    
+    e := &Cmd{
+        pool:      pool,
+        cmdstr:    cmdstr,
+        restart:   restart,
+        env:       env,
+        onExit:    onExit,
+        terminate: make(chan struct{}),
+        ctx:       ctx,
+        cancel:    cancel,
+        running:   true,
+    }
+    
+    pool.wg.Add(1)
+    go e.run()
+    return e
 }
 
 // Close closes the command. It doesn't wait for the command to exit.
 func (e *Cmd) Close() {
-	close(e.terminate)
+    e.mu.Lock()
+    if !e.running {
+        e.mu.Unlock()
+        return
+    }
+    e.running = false
+    e.mu.Unlock()
+    
+    e.cancel()  // Cancel the context
+    close(e.terminate)
 }
 
 func (e *Cmd) run() {
-	defer e.pool.wg.Done()
-
-	env := append([]string(nil), os.Environ()...)
-	for key, val := range e.env {
-		env = append(env, key+"="+val)
-	}
-
-	for {
-		err := e.runOSSpecific(env)
-		if errors.Is(err, errTerminated) {
-			return
-		}
-
-		if !e.restart {
-			if err != nil {
-				e.onExit(err)
-			}
-			return
-		}
-
-		if err != nil {
-			e.onExit(err)
-		} else {
-			e.onExit(fmt.Errorf("command exited with code 0"))
-		}
-
-		select {
-		case <-time.After(restartPause):
-		case <-e.terminate:
-			return
-		}
-	}
+    defer e.pool.wg.Done()
+    defer e.cancel()  // Ensure context is cancelled when we exit
+    
+    env := append([]string(nil), os.Environ()...)
+    for key, val := range e.env {
+        env = append(env, key+"="+val)
+    }
+    
+    for {
+        select {
+        case <-e.ctx.Done():
+            return
+        default:
+            err := e.runOSSpecific(env)
+            if errors.Is(err, errTerminated) {
+                return
+            }
+            
+            if !e.restart {
+                if err != nil {
+                    e.onExit(err)
+                }
+                return
+            }
+            
+            if err != nil {
+                e.onExit(err)
+            } else {
+                e.onExit(fmt.Errorf("command exited with code 0"))
+            }
+            
+            select {
+            case <-time.After(restartPause):
+            case <-e.terminate:
+                return
+            case <-e.ctx.Done():
+                return
+            }
+        }
+    }
 }
--- internal/externalcmd/cmd_unix.go
+++ internal/externalcmd/cmd_unix.go
@@ -3,61 +3,37 @@
 package externalcmd
 
 import (
-	"errors"
-	"fmt"
-	"os"
-	"os/exec"
-	"syscall"
-
-	"github.com/kballard/go-shellquote"
+    "os/exec"
+    "syscall"
+    "time"
 )
 
 func (e *Cmd) runOSSpecific(env []string) error {
-	cmdParts, err := shellquote.Split(e.cmdstr)
-	if err != nil {
-		return err
-	}
-
-	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
-
-	cmd.Env = env
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	// set process group in order to allow killing subprocesses
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-
-	err = cmd.Start()
-	if err != nil {
-		return err
-	}
-
-	cmdDone := make(chan int)
-	go func() {
-		cmdDone <- func() int {
-			err := cmd.Wait()
-			if err == nil {
-				return 0
-			}
-			var ee *exec.ExitError
-			if errors.As(err, &ee) {
-				ee.ExitCode()
-			}
-			return 0
-		}()
-	}()
-
-	select {
-	case <-e.terminate:
-		// the minus is needed to kill all subprocesses
-		syscall.Kill(-cmd.Process.Pid, syscall.SIGINT) //nolint:errcheck
-		<-cmdDone
-		return errTerminated
-
-	case c := <-cmdDone:
-		if c != 0 {
-			return fmt.Errorf("command exited with code %d", c)
-		}
-		return nil
-	}
+    cmd := exec.Command("sh", "-c", e.cmdstr)
+    cmd.Env = env
+    
+    err := cmd.Start()
+    if err != nil {
+        return err
+    }
+    
+    done := make(chan error)
+    go func() {
+        done <- cmd.Wait()
+    }()
+    
+    select {
+    case err := <-done:
+        return err
+        
+    case <-e.terminate:
+        cmd.Process.Signal(syscall.SIGTERM)
+        select {
+        case <-done:
+        case <-time.After(5 * time.Second):
+            cmd.Process.Kill()
+            <-done
+        }
+        return errTerminated
+    }
 }
